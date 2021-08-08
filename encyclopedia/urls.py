723904bbe56d16edda0a8c4177acd36432f4c90b
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-08-08 21:51:05 +0100

    added markdown to html parser

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.detail, name="detail"),
-    path("wiki/<str:title>", views.random_page, name="random"),
+    path("random/<str:title>", views.random_page, name="random"),
     path("error", views.error_404, name="error_page"),
     path("create", views.create, name="create"),
     path("search", views.search_result, name="search"),
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.urls import reverse_lazy
 from .forms import CreateForm, SearchForm
-import random
+from markdown2 import Markdown
+import random, re
 
 from . import util
 
@@ -15,9 +16,10 @@ def index(request):
 
 def detail(request, title):
     entry = util.get_entry(title)
-    if entry == None:
+    parser = Markdown()
+    if entry is None:
         return redirect(reverse_lazy('error_page'))
-    return render(request, "encyclopedia/title.html", {"entry": entry})
+    return render(request, "encyclopedia/title.html", {"entry": parser.convert(entry)})
 
 
 def create(request):
@@ -51,11 +53,28 @@ def search_result(request):
         return render(request, "encyclopedia/layout.html", {"form": SearchForm()})
 
 
-def random_page(request):
-    entries = util.list_entries()
-    entry = random.choice(entries)
-    return render(request, "encyclopedia/layout.html", {'entry': entry})
+def random_page(request, title):
+    # entries = util.list_entries()
+    # entry = util.get_entry(random.choice(entries))
+    # return render(request, "encyclopedia/layout.html", {'entry': entry})
+    pass
 
 
 def error_404(request):
     return render(request, "encyclopedia/error.html")
+
+
+def parser(mdText):
+    htmlText = re.sub(r'^# (.*$)', r"<h1>\1</h1>", mdText, flags=re.I | re.M)
+    htmlText2 = re.sub(r'## (.*$)', r"<h2>\1</h2>", htmlText, flags=re.M | re.I)
+    htmlText3 = re.sub(r'^### (.*$)', r"<h3>\1</h3>", htmlText2, flags=re.M | re.I)
+    htmlText4 = re.sub(r'\*\*(.*)\*\*', r"<b>\1</b>", htmlText3, flags=re.M | re.I)
+    htmlText5 = re.sub(r'\*(.*)\*', r"<i>\1</i>", htmlText4, flags=re.M | re.I)
+    htmlText6 = re.sub(r'\~\~(.*)\~\~', r"<del>\1</del>", htmlText5, flags=re.M | re.I)
+    htmlText7 = re.sub(r'^\> (.*)', r"<blockquote>\1</blockquote>", htmlText6, flags=re.M | re.I)
+    htmlText8 = re.sub(r'!\[(.*?)\]\((.*?)\)', r"<img alt='\1' src='\2' />", htmlText7, flags=re.M | re.I)
+    htmlText9 = re.sub(r'\[(.*?)\]\((.*?)\)', r"<a href='\2'>\1</a>", htmlText8, flags=re.M | re.I)
+    htmlText10 = re.sub(r'^\*|\- (.*?)', r"<li>\1</li>", htmlText9, flags=re.M | re.I)
+
+    return htmlText10
+
