b8e9f63a6d9f395c6afb225d58c008e92353c473
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-08-08 02:41:38 +0100

    added error page and entries link to detail

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.detail, name="detail"),
-    path("error_page/<str:title>", views.error_404, name="error_page"),
-]
+    path("error/<str:title>", views.error_404, name="error_page"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
